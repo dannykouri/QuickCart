1d58b2f08a4d5f599277b4a7c3cc8ca9178beca6
dannykouri <ho271208@gmail.com>
2025-04-16 16:28:23 +0700

    bruh 34

---
+++ app/api/user/data/route.js
@@ -0,0 +1,23 @@
+import User from "@/models/Users";
+import { getAuth} from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import connectDB from "@/config/db";
+
+
+export async function GET(request) {
+    try {
+        
+        const {userId}=getAuth(request)
+
+        await connectDB()
+        const user = await User.findById(userId)
+        if(!user){
+            return NextResponse.json({success:false, message:"User not found"}, {status:404})
+        }
+        return NextResponse.json({success:true, user}, {status:200})
+
+
+    } catch (error) {
+        return NextResponse.json({success:false, message:error.message}, {status:404})
+    }
+}
--- app/layout.js
+++ app/layout.js
@@ -7,8 +7,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
 
 export const metadata = {
-  title: "QuickCart - GreatStack",
-  description: "E-Commerce with Next.js ",
+  title: "Nhà Sách Nhật Minh",
+  description: "Sách trong tầm tay bạn",
 };
 
 export default function RootLayout({ children }) {
