817564327380b8c08395749c9d036ea01d2bdd24
dannykouri <ho271208@gmail.com>
2025-04-16 09:52:09 +0700

    bruh 33

--- config/inngest.js
+++ config/inngest.js
@@ -1,5 +1,4 @@
 import { Inngest } from "inngest";
-import { connect } from "mongoose";
 import connectDB from "./db";
 import User from "@/models/Users";
 
@@ -7,49 +6,53 @@ import User from "@/models/Users";
 export const inngest = new Inngest({ id: "nhatminh-next" });
 
 export const syncUserCreation = inngest.createFunction(
-  { id : "syncUsercreation"},
-  { 
-    event: "clerk/user.created"
+  { id: "syncUserCreation" },
+  {
+    event: "clerk/user.created",
   },
   async ({ event }) => {
-    const { id, first_name,last_name,email_addresses,image_url } = event.data;
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
+
     const user = {
-      _id:id,
-      name: first_name+" "+last_name,
-      email_addresses: email_addresses[0].email_address,
-      image_url:image_url,
+      _id: id,
+      name: `${first_name} ${last_name}`,
+      email: email_addresses[0].email_address, // đúng với schema
+      imageUrl: image_url,                      // đúng với schema
     };
+
     await connectDB();
-    await User.create(userData);
+    await User.create(user); // ✅ sửa userData → user
   }
 );
 
 export const syncUserUpdation = inngest.createFunction(
-  { id : "syncUserUpdation"},
+  { id: "syncUserUpdation" },
   {
-    event: "clerk/user.updated"
+    event: "clerk/user.updated",
   },
   async ({ event }) => {
-    const { id, first_name,last_name,email_addresses,image_url } = event.data;
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
+
     const user = {
-      _id:id,
-      name: first_name+" "+last_name,
-      email_addresses: email_addresses[0].email_address,
-      image_url:image_url,
+      _id: id,
+      name: `${first_name} ${last_name}`,
+      email: email_addresses[0].email_address,
+      imageUrl: image_url,
     };
+
     await connectDB();
-    await User.findByIdAndUpdate(id, userData);
+    await User.findByIdAndUpdate(id, user); // ✅ sửa userData → user
   }
-)
+);
 
 export const syncUserDeletion = inngest.createFunction(
-  { id : "syncUserDeletion"},
-  { 
-    event: "clerk/user.deleted"
+  { id: "syncUserDeletion" },
+  {
+    event: "clerk/user.deleted",
   },
   async ({ event }) => {
     const { id } = event.data;
     await connectDB();
     await User.findByIdAndDelete(id);
   }
-)
+);
